52c370db3c114ed989af2726a224e603112ee5d2
Git-Prakhar <kprakhar01@gmail.com>
2025-04-12 22:32:25 +0530

    fix: improve performance on mobile to reduce lag

--- src/app/LandingPage/page.tsx
+++ src/app/LandingPage/page.tsx
@@ -3,15 +3,38 @@ import Navbar from "./Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
 import Contact from "./Contact";
+import { useEffect, useState } from "react";
 
 
-export default function page() {
+export default function Page() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleResize = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    handleResize(); // Check on initial load
+    window.addEventListener("resize", handleResize); // Add event listener for window resize
+    console.log("Window resized:", window.innerWidth); // Log the window width on resize
+    console.log("Is mobile:", isMobile); // Log the mobile state
+
+    return () => {
+
+      window.removeEventListener("resize", handleResize); // Cleanup event listener on unmount
+    };
+
+  }, []);
   return (
-    <>
-    <Navbar/>
-      <Hero/>
-      <Features />
+    <div className="flex flex-col relative">
+      <Navbar />
+      <Hero isMobile={isMobile} />
+      <Features isMobile={isMobile} />
       <Contact />
-    </>
+    </div>
   );
 }
